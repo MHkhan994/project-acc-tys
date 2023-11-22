46d35716fa98c22ddc927592b2bfea89581d7644
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-11-22 16:23:55 +0600

    responsive nav

--- app/product/[id]/ProductDetails.tsx
+++ app/product/[id]/ProductDetails.tsx
@@ -35,9 +35,9 @@ const ProductDetails = ({ product }: { product: Product | undefined }) => {
             <div>
                 {
                     currentTab !== 'picture_gallery' && <>
-                        <p className='primary-p pt-[30px]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-                        <p className='primary-p pt-[30px]'>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                        <p className='primary-p pt-[30px]'>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        <p className='primary-p pt-[30px]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &apos;Content here, content here&apos;, making it look like readable English.</p>
+                        <p className='primary-p pt-[30px]'>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &apos;lorem ipsum&apos; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        <p className='primary-p pt-[30px]'>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &apos;lorem ipsum&apos; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                     </>
                 }
                 {
